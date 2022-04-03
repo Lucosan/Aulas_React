0264832b6736ac13aadc9ad99b28b28624ed2f15
Lucosan <luccacostasantosjob@gmail.com>
2022-04-03 19:47:47 -0300

    Multiplicador de Pessoas

---
+++ multiplicador/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View, TextInput, Pressable } from 'react-native';
+import { styles } from './Styles';
+
+export default function App() {
+  const [numeroUm, setnumeroUm] = React.useState(null);
+  const [numeroDois, setnumeroDois] = React.useState(null);
+  const [resultado, setResultado] = React.useState('');
+  return (
+    <View style={styles.container}>
+    <Text style={styles.nomeApp}>Multiplicador de Números</Text>
+
+    {/*Tive uma dificuldade para usar o keyboardtype, ficava dando erro. Tentei usar um Styled Input, mas ainda não peguei a sintaxe. Meu colega Helio me ajudou me mostrando como usar regex.*/}
+
+    <TextInput style={styles.inputs} placeholderTextColor="#555"  placeholder='Digite um número:' keyboardType='numeric' value={numeroUm} onChangeText={texto=>setnumeroUm(texto.replace(/[^0-9]/g, ''))}/>
+    <TextInput style={styles.inputs} placeholderTextColor="#555" placeholder='Digite o outro número:' keyboardType='numeric' value={numeroDois} onChangeText={texto=>setnumeroDois(texto.replace(/[^0-9]/g, ''))}/>
+    
+    {/*Fazendo na ordem ao contrário pra dar uma originalidade, como a ordem dos fatores não altera o produto, nesse caso...*/}
+
+    <Pressable style={styles.button} onPress={()=>setResultado(numeroDois*numeroUm)}>
+      <Text style={styles.calc}>
+        Calcular
+      </Text>
+    </Pressable>
+    <View>
+      <Text style={styles.results}>
+       Seu resultado é: {resultado}
+      </Text>
+    </View>
+    </View>
+  );
+}
---
+++ multiplicador/Styles.js
@@ -0,0 +1,40 @@
+import {StyleSheet} from 'react-native';
+ 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    calc: {
+      color: '#000',
+      fontSize: 32,
+    },
+    nomeApp:{
+      color: '#000',
+      fontSize: 32,
+      textAlign: 'center',
+      marginTop: 48,
+    },
+    button: {
+      backgroundColor: '#0fa',
+      margin: 20,
+      borderRadius: 40,
+      fontSize: 30,
+      alignItems: 'center',
+      padding: 10,
+    },
+    inputs: {
+      padding: 10,
+      color: '#000',
+      marginVertical: 16,
+      fontSize: 24,
+      borderColor: '#999',
+      borderWidth: 2,
+    },
+    results: {
+        textAlign: 'center',
+        fontSize: 32,
+        color: '#567',
+    }
+  });
+export{styles};
